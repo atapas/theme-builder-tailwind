355efeb4da5db53bca1953ff9c163ffb8e9a9542
Tapas Adhikary <tapas.adhikary@gmail.com>
2023-02-09 23:02:22 +0530

    Populating basic theme names

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
+import {useState} from 'react'
 import './App.css'
+import data from './theme/schema.json'
 
 function App() {
-
+  console.log(data.themes);
+  const [themes, setThemes] = useState(data.themes);
   return (
-    <h1 className='text-3xl font-bold underline'>Hello World!</h1>
+    <div className="flex flex-row justify-center align-middle">
+      {
+        themes && Reflect.ownKeys(themes).map((themeName) => (
+          <div className="border-2 border-blue-50 p-3 m-3 rounded-md">
+            {themeName}
+          </div>
+        ))
+      }
+    </div>
   )
 }
 
